cd353c90349db9f42ec6e5dd691501837b687969
Corfaya <lauracorrado0696@gmail.com>
2024-09-26 15:45:40 +0200

    [UPD] columns created on migration

--- database/migrations/2024_09_26_133319_create_trains_table.php
+++ database/migrations/2024_09_26_133319_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 30);
+            $table->string('Stazione di partenza', 50);
+            $table->string('Stazione di arrivo', 50);
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->string('Codice Treno', 20);
+            $table->tinyInteger('Numero Carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
             $table->timestamps();
         });
     }
